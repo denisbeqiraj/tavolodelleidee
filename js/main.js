afe8bc4e6c5b19acfe25834f5e23ec5a3abebf6d
Beqiraj Denis <denis.beqiraj@student.supsi.ch>
2022-03-08 13:54:01 +0100

    Changed main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Create WebSocket connection.
-/*const socket = new WebSocket('ws://localhost:8765');
-
+const socket = new WebSocket('ws://localhost:8765');
+var API_KEY = '26044387-30ea27ccb218f7f03d57137ae';
+var url = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+var elementimage = document.querySelector("#box");
 // Connection opened
 socket.addEventListener('open', function (event) {
     socket.send('Start');
@@ -10,15 +12,13 @@ socket.addEventListener('open', function (event) {
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
     socket.send('Received');
-});*/
-//var API_KEY = '26044387-30ea27ccb218f7f03d57137ae';
-//var url = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-var elementimage=document.getElementById('elementimage');
-elementimage.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
-/*fetch(url)
-    .then(response => {
-        console.log(response.json());
-    })
-    .catch(error => {
-        // handle the error
-    });*/
+    elementimage.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
+    fetch(url)
+        .then(response => {
+            var resp=response.json();
+            console.log(resp["hits"]);
+        })
+        .catch(error => {
+            // handle the error
+    });
+});
