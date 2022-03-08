d1f5874441083e3f8dce73e8bda35a6616e2b999
Beqiraj Denis <denis.beqiraj@student.supsi.ch>
2022-03-08 13:36:58 +0100

    Added js html

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8765');
+/*const socket = new WebSocket('ws://localhost:8765');
 
 // Connection opened
 socket.addEventListener('open', function (event) {
@@ -10,4 +10,15 @@ socket.addEventListener('open', function (event) {
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
     socket.send('Received');
-});
+});*/
+//var API_KEY = '26044387-30ea27ccb218f7f03d57137ae';
+//var url = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+var elementimage=document.getElementById('elementimage');
+elementimage.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
+/*fetch(url)
+    .then(response => {
+        console.log(response.json());
+    })
+    .catch(error => {
+        // handle the error
+    });*/
