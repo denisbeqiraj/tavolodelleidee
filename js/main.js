5030180ea48d639611e28452c3b881a789c5746a
Beqiraj Denis <denis.beqiraj@student.supsi.ch>
2022-03-15 14:03:04 +0100

    Added search api by filter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // Create WebSocket connection.
 const socket = new WebSocket('ws://localhost:8765');
 var API_KEY = '26044387-30ea27ccb218f7f03d57137ae';
-var url = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
 var elementimage = document.querySelector("#box");
 // Connection opened
 socket.addEventListener('open', function (event) {
@@ -12,14 +11,16 @@ socket.addEventListener('open', function (event) {
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
     socket.send('Received');
-    elementimage.insertAdjacentHTML('beforeend', '<div id="two">two</div>');
-    fetch(url)
+    var split_word=event.data.split(",")
+    if(split_word.length != 1){
+        var url = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(split_word[0]);
+        fetch(url)
         .then(response => {
             return response.json();
         }).then(data => {
-            console.log(data["hits"][0]["largeImageURL"]);
         })
         .catch(error => {
             // handle the error
-    });
+        });
+    }
 });
