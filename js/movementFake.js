0e31aedab48d060ccda1a1484f879b308b01547d
Andaloro Natalia <natalia.andaloro@student.supsi.ch>
2022-04-24 14:57:32 +0200

    Fix cube movement

--- js/movementFake.js
+++ js/movementFake.js
@@ -1,56 +1,13 @@
-function move(images){
-
-    const el = document.querySelector("#box");
-    //const imm = document.querySelector('#img');
-    //imm.src=images;
-
-    //el.style.height = innerHeight/4 + "px";
-    //el.style.width = innerWidth/4 + "px";
-
-    /*In questo modo l'immagine ha un limite di altezza che dipende dall'altezza del div box
-    if(imm.naturalHeight > el.clientHeight){
-        imm.style.height = el.clientHeight + "px";
-    }*/
-
-    //dimensioni della finestra
-    //alert(innerWidth); 1280
-    //alert(innerHeight); 577
-
-    var timings = {
-        duration: 1500
-    };
-
-    var keyframes = [
-        {
-            transform: 'translateZ(-100000px)',
-        },
-        {
-            transform: 'translateZ(-100px)',
-        }
-    ];
-
-    var timings2 = {
-        duration: 12000,
-        fill: 'both'
-    };
-
-    var keyframes2 = [
-        {
-            offsetDistance: '0%'
-        },
-        {
-            offsetDistance: '100%'
-        }
-    ];
-
-    var effects = [
-        new KeyframeEffect(el,keyframes,timings),
-        //new KeyframeEffect(el,keyframes2,timings2)
-    ];
-
-    var sequence = new SequenceEffect(effects);
-
-    document.timeline.play(sequence);
 
 
-};
+const img = document.getElementById("imm");
+img.addEventListener("error", myFunction());
+
+function myFunction() {
+  var text = document.createElement("div");
+  text.className = "text";
+  text.innerHTML = "Lavoro di semestre di Natalia Andaloro e Denis Beqiraj";
+  var parent = img.parentNode;
+  parent.insertBefore(text,img);
+  parent.removeChild(img);
+}
