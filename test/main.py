a2ff96a087695e119ed37aea27e0861aff2fa838
Andaloro Natalia <natalia.andaloro@student.supsi.ch>
2022-05-10 00:39:06 +0200

    Fix error

--- test/main.py
+++ test/main.py
@@ -1,6 +1,4 @@
 import speech_recognition as sr
-import asyncio
-import websockets
 import yake
 import spacy
 import requests
--- test/static/movement.js
+++ test/static/movement.js
@@ -1,20 +1,59 @@
-function move(images, word){
+const img2 = document.getElementById("img2");
+img2.addEventListener("error", myFunction(img2));
 
-    const img2 = document.getElementById("img2");
-    console.log(img2);
-    img2.addEventListener("error", myFunction());
-
-    function myFunction() {
+function myFunction(img2) {
         const text = document.createElement("div");
         text.className = "text";
         text.innerHTML = "Lavoro di semestre di Natalia Andaloro e Denis Beqiraj";
+        text.id = "text";
         const parent = img2.parentNode;
         parent.insertBefore(text,img2);
         parent.removeChild(img2);
     }
+
+const img1 = document.querySelector('#img1');
+img1.addEventListener("error", function(){
+    img1.src = "../static/logo.png";
+})
+const img3 = document.querySelector('#img3');
+img3.addEventListener("error", function(){
+    img3.src = "../static/supsi_bianca.jfif";
+})
+const img4 = document.querySelector('#img4');
+img4.addEventListener("error", function(){
+    img4.src = "../static/StartUpGarage.png";
+})
+const img5 = document.querySelector('#img5');
+img5.addEventListener("error", function(){
+    img5.src = "../static/logo.png";
+});
+
+function move(images, word){
+
+    const box = document.querySelector("#box");
+    setTimeout(function(){
+        if(box.classList.contains('animate')){
+            box.classList.remove('animate');
+        }
+    }, 200000);
+    let img2 = document.querySelector("#img2");
+    if(img2){
+        img2.src=images[1];
+    } else {
+        const parent = document.querySelector("#back");
+        img2 = document.createElement("img");
+        img2.id = "img2";
+        const text = document.querySelector("#text");
+        parent.insertBefore(img2,text);
+        parent.removeChild(text);
+        img2.src=images[1];
+        img2.width = "150";
+        img2.height = "150";
+    }
+    img2.addEventListener("error", myFunction(img2));
+
     const img1 = document.querySelector('#img1');
     img1.src=images[0];
-    img2.src=images[1];
     const img3 = document.querySelector('#img3');
     img3.src=images[2];
     const img4 = document.querySelector('#img4');
@@ -23,7 +62,6 @@ function move(images, word){
     img5.src=images[4];
     const words = document.querySelector('#word');
     words.textContent=word;
-    const box = document.querySelector("#box");
     box.classList.add('animate');
 
 }
