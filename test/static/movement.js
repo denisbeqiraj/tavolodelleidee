7fccbcc2568a856ba85bf18c8c37546af5326e2a
Andaloro Natalia <natalia.andaloro@student.supsi.ch>
2022-05-10 13:48:33 +0200

    Fix bug movement

--- test/static/movement.js
+++ test/static/movement.js
@@ -1,67 +1,96 @@
-const img2 = document.getElementById("img2");
-img2.addEventListener("error", myFunction(img2));
+const firstBox = document.querySelector("#firstBox");
+firstBox.addEventListener("animationend", deleteBox);
 
-function myFunction(img2) {
+function deleteBox(){
+    this.remove();
+}
+
+function move(images, word){
+
+    const parent = document.querySelector(".scene");
+
+    const newBox = document.createElement("div");
+    newBox.className = "box animate";
+
+    const front = document.createElement("div");
+    front.className = "box__face box__face--front text";
+    const wordNode = document.createTextNode(word);
+    front.appendChild(wordNode);
+
+    newBox.appendChild(front);
+
+    const back = document.createElement("div");
+    back.className = "box__face box__face--back";
+    const img0 = document.createElement("img");
+    img0.src=images[0];
+    img0.width = "150";
+    img0.height = "150";
+    img0.addEventListener("error", function() {
         const text = document.createElement("div");
         text.className = "text";
         text.innerHTML = "Lavoro di semestre di Natalia Andaloro e Denis Beqiraj";
         text.id = "text";
-        const parent = img2.parentNode;
-        parent.insertBefore(text,img2);
-        parent.removeChild(img2);
-    }
+        const parent = this.parentNode;
+        parent.insertBefore(text,this);
+        parent.removeChild(this);
+    });
+    back.appendChild(img0);
 
-const img1 = document.querySelector('#img1');
-img1.addEventListener("error", function(){
-    img1.src = "../static/logo.png";
-})
-const img3 = document.querySelector('#img3');
-img3.addEventListener("error", function(){
-    img3.src = "../static/supsi_bianca.jfif";
-})
-const img4 = document.querySelector('#img4');
-img4.addEventListener("error", function(){
-    img4.src = "../static/StartUpGarage.png";
-})
-const img5 = document.querySelector('#img5');
-img5.addEventListener("error", function(){
-    img5.src = "../static/logo.png";
-});
+    newBox.appendChild(back);
 
-function move(images, word){
+    const right = document.createElement("div");
+    right.className = "box__face box__face--right";
+    const img1 = document.createElement("img");
+    img1.src=images[1];
+    img1.width = "150";
+    img1.height = "150";
+    img1.addEventListener("error", function(){
+        img1.src = "../static/supsi_bianca.jfif";
+    });
+    right.appendChild(img1);
 
-    const box = document.querySelector("#box");
-    setTimeout(function(){
-        if(box.classList.contains('animate')){
-            box.classList.remove('animate');
-        }
-    }, 200000);
-    let img2 = document.querySelector("#img2");
-    if(img2){
-        img2.src=images[1];
-    } else {
-        const parent = document.querySelector("#back");
-        img2 = document.createElement("img");
-        img2.id = "img2";
-        const text = document.querySelector("#text");
-        parent.insertBefore(img2,text);
-        parent.removeChild(text);
-        img2.src=images[1];
-        img2.width = "150";
-        img2.height = "150";
-    }
-    img2.addEventListener("error", myFunction(img2));
+    newBox.appendChild(right);
 
-    const img1 = document.querySelector('#img1');
-    img1.src=images[0];
-    const img3 = document.querySelector('#img3');
-    img3.src=images[2];
-    const img4 = document.querySelector('#img4');
-    img4.src=images[3];
-    const img5 = document.querySelector('#img5');
-    img5.src=images[4];
-    const words = document.querySelector('#word');
-    words.textContent=word;
-    box.classList.add('animate');
+    const left = document.createElement("div");
+    left.className = "box__face box__face--left";
+    const img2 = document.createElement("img");
+    img2.src=images[2];
+    img2.width = "150";
+    img2.height = "150";
+    img2.addEventListener("error", function(){
+        img2.src = "../static/StartUpGarage.png";
+    });
+    left.appendChild(img2);
 
+    newBox.appendChild(left);
+
+    const top = document.createElement("div");
+    top.className = "box__face box__face--top";
+    const img3 = document.createElement("img");
+    img3.src=images[3];
+    img3.width = "150";
+    img3.height = "150";
+    img3.addEventListener("error", function(){
+        img3.src = "../static/logo.png";
+    });
+    top.appendChild(img3);
+
+    newBox.appendChild(top);
+
+    const bottom = document.createElement("div");
+    bottom.className = "box__face box__face--bottom";
+    const img4 = document.createElement("img");
+    img4.src=images[4];
+    img4.width = "150";
+    img4.height = "150";
+    img4.addEventListener("error", function(){
+        img4.src = "../static/logo.png";
+    });
+    bottom.appendChild(img4);
+
+    newBox.appendChild(bottom);
+
+    newBox.addEventListener("animationend", deleteBox);
+    const end = document.querySelector(".end");
+    parent.insertBefore(newBox,end);
 }
