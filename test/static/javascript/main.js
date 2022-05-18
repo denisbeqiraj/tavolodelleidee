d89651e9b23a821656ea754c6230b634b0ceb962
Beqiraj Denis <denis.beqiraj@student.supsi.ch>
2022-05-18 10:02:40 +0200

    Added comments

--- test/static/javascript/main.js
+++ test/static/javascript/main.js
@@ -1,10 +1,14 @@
 var socket = io();
+//Connessione websocket
 socket.on('connect', function() {
     socket.emit('tavolodelleidee', {data: 'I\'m connected!'});
 });
+//Setup ogni richiesta
 socket.on('response', function(msg) {
+//parso il json in input
     const split_word=JSON.parse(msg);
-    console.log(split_word["all_data"]);
+    //console.log(split_word["all_data"]);
+    //Se ci sono parole aggiungo
     if(split_word["all_data"]["link"].length>0){
         move(split_word["all_data"]["link"], split_word["all_data"]["word"][0]);
     }
